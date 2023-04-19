9e17bc04ab1d88a2c9f2288fe7632bf9ac617d4b
Nahzilian <nahz3112@gmail.com>
2023-04-19 19:54:18 -0400

    Update code

---
+++ templates/website/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    colors: {
+      "jet": "#363636",
+      "gunmetal": "#242F40",
+      "satin-sheen-gold": "#CCA43B",
+      "platnium": "#E5E5E5",
+      "white": "#FFFFFF"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
